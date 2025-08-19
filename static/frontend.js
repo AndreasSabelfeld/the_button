2cfccd906bd00227744661cab98fe2fb4d5328f5
AndreasSabelfeld <and.sabelfeld@gmail.com>
2025-08-19 19:47:32 +0200

    fixed a typo

--- static/frontend.js
+++ static/frontend.js
@@ -1,7 +1,7 @@
 // Author: Andreas Sabelfeld
 // Date: Tue 19th August 2025
 
-const socket = io("httos://https://the-button-qqsp.onrender.com/");
+const socket = io("https://the-button-qqsp.onrender.com/");
 
 const button = document.getElementById("theButton");
 
